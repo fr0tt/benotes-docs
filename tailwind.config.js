e346023c2f5368481fbe353b8f9e69d7fe2f468b
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-07-15 15:26:27 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,md,ts,vue}'],
+        theme: {
+                extend: {
+                        fontFamily: {
+                                'sans': 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Ca$                        },
+                },
+        },
+  plugins: [],
+}
