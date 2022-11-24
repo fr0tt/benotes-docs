15817761059ad0d852f5fbba3400dca8297a4dc3
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-11-24 22:46:37 +0100

    add head elements

--- app.vue
+++ app.vue
@@ -1,7 +1,27 @@
+<script setup lang="ts">
+    useHead({
+        titleTemplate: (titleChunk) => {
+            return titleChunk ? 'Benotes - ' + titleChunk : 'Benotes';
+        }
+    })
+</script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+
+    <Head>
+        <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <Meta name="description" content="Benotes. Your new open-source bookmark and notes companion" />
+    </Head>
+
+    <Body>
+        <div>
+            <NuxtLayout>
+                <NuxtPage />
+            </NuxtLayout>
+        </div>
+    </Body>
+
 </template>
--- components/Headline.vue
+++ components/Headline.vue
@@ -1,3 +1,13 @@
+<script setup>
+const slots = useSlots()
+if (slots !== null) {
+    console.log(slots.default?.())
+    useHead({
+      title: slots.default?.()[0].children
+    })
+}
+</script>
+
 <template>
     <h1 class="text-2xl my-6 font-semibold">
         <slot />
