48cf7e0c24cd4f409596567de1b5f7f86d51e248
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-11-24 22:45:31 +0100

    add home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
-<template>
+<script setup>
+definePageMeta({
+    layout: 'home',
+});
+</script>
 
+<template>
+    <div class="text-center">
+        <h1 class="text-5xl font-bold my-2 text-gradient">Benotes: Notes & Bookmarks</h1>
+        <h2 class="text-4xl font-bold my-2 text-white">Save all your information in</h2>
+        <h2 class="text-4xl font-bold my-2 text-gradient">one place.</h2>
+        <a href="/docs" class="inline-block my-12 px-6 py-3 text-2xl
+                tracking-wide text-white bg-gradient-to-r from-orange-500 to-pink-500
+                font-bold rounded-lg">Get started</a>
+        <img class="mx-auto w-8/12" src="/demo_composed_screenshots.png" alt="Benotes Screenshot"/>
+    </div>
 </template>
+
+<style>
+    .text-gradient {
+        @apply bg-clip-text text-transparent bg-gradient-to-r from-orange-600 to-purple-700;
+    }
+</style>
