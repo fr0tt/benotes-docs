289a8724eb3d6cf2317276429d70f901ccb81112
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-11-27 15:47:02 +0100

    fix sidebar again

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,8 @@
     }
 
     function outsideSidebar (event) {
+        if (window.matchMedia('(min-width: 768px)').matches)
+            return
         if (event.target.contains(document.querySelector('#sidebar')))
             return
         document.querySelector('#sidebar').style.display = 'none'
@@ -58,7 +60,7 @@
         </div>
     </header>
 
-    <main class="flex px-4" @click="window.matchMedia('(max-width: 768px)') && outsideSidebar">
+    <main class="flex px-4" @click="outsideSidebar">
         <aside id="sidebar" class="fixed top-0 left-0 h-full md:relative hidden md:block w-80 px-12 py-12
             bg-white shadow-lg md:shadow-none z-50 transition-all duration-700">
             <section v-for="section in config.sidebar" :key="section.name" class="mb-4">
@@ -86,8 +88,5 @@
   }
   .router-link-active {
     @apply text-orange-500;
-  }
-  #sidebar {
-    transition: display 1s;
   }
 </style>
