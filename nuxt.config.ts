2cbb15211dec5cb09e95e4dcfba50563aec59d26
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-11-27 16:03:44 +0100

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@funken-studio/sitemap-nuxt-3'
+    ],
+    sitemap: {
+        hostname: 'https://benotes.org',
+    },
     // only available server-side
     runtimeConfig: {
     }
