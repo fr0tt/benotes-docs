d54f32f55a091077a68224a00a4469ad10bc99da
fr0tt <33751346+fr0tt@users.noreply.github.com>
2022-11-28 18:44:32 +0100

    make homepage more responsive

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,15 @@ definePageMeta({
 </script>
 
 <template>
-    <div class="text-center">
-        <h1 class="text-5xl font-bold my-2 text-gradient">Benotes: Notes & Bookmarks</h1>
-        <h2 class="text-4xl font-bold my-2 text-white">Save all your information in</h2>
-        <h2 class="text-4xl font-bold my-2 text-gradient">one place.</h2>
+    <div class="pt-2 text-center">
+        <h1 class="text-4xl md:text-5xl font-bold my-2 text-gradient">Benotes: Notes & Bookmarks</h1>
+        <h2 class="text-3xl md:text-4xl font-medium my-4 text-white">Save all your information in</h2>
+        <h2 class="text-3xl md:text-4xl font-bold my-2 text-gradient">one place.</h2>
         <a href="/docs" class="inline-block my-12 px-6 py-3 text-2xl
                 tracking-wide text-white bg-gradient-to-r from-orange-500 to-pink-500
                 font-bold rounded-lg">Get started</a>
-        <img class="mx-auto w-8/12" src="/demo_composed_screenshots.png" alt="Benotes Screenshot"/>
+        <img class="mx-auto w-11/12 md:w-8/12"
+            src="/demo_composed_screenshots.png" alt="Benotes Screenshot"/>
     </div>
 </template>
 
