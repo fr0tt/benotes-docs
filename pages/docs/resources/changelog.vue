f524f2ae20991b7ddfba03a8ffa69ab6029e3cf8
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-07-28 16:03:52 +0200

    update changelog

--- pages/docs/resources/changelog.vue
+++ pages/docs/resources/changelog.vue
@@ -1,6 +1,19 @@
 <template>
-
     <Headline>Changelog</Headline>
+
+    <SubHeadline>2.7.1</SubHeadline>
+    <ul>
+        <li>fix lower case search with PostgreSQL</li>
+        <li>include child-collections in search</li>
+        <li>fix screenshot taking</li>
+    </ul>
+
+    <SubHeadline>2.7.0</SubHeadline>
+    <ul>
+        <li>docker image has now multiplatform support</li>
+        <li>PostgreSQL as new default database (with docker compose)</li>
+        <li>screenshots to replace missing thumbnails, both manually and automated in a certain interval</li>
+    </ul>
 
     <SubHeadline>2.6.0</SubHeadline>
     <ul>
@@ -23,7 +36,8 @@
         <li>implement Playwright with some browser tests (after spending many hours with it and Cypress and Dusk..)</li>
         <li>simplify docker-compose setup</li>
         <li>update Dockerfile</li>
-        <li>use fonts locally instead of a CDN, as request by <a href="https://github.com/fr0tt/benotes/issues/45"><code>@MohamedElashri</code></a></li>
+        <li>use fonts locally instead of a CDN, as request by <a
+                href="https://github.com/fr0tt/benotes/issues/45"><code>@MohamedElashri</code></a></li>
     </ul>
 
     <SubHeadline>2.3.1</SubHeadline>
@@ -31,7 +45,7 @@
         <li>fix bug: add line again that was removed accidentally the day before</li>
         <li>fix: redirect bug</li>
         <li>fix: (text and bookmarks) post creation with tags</li>
-		<li>fixed: bottommenu on mobile can be scrolled away</li>
+        <li>fixed: bottommenu on mobile can be scrolled away</li>
     </ul>
 
     <SubHeadline>2.3.0</SubHeadline>
@@ -39,5 +53,4 @@
         <li>complete tag implementation</li>
         <li>fix key-generate command</li>
     </ul>
-
 </template>
