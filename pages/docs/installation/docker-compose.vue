8e20278379835d0391ec9a1c8d5376c67729d489
fr0tt <33751346+fr0tt@users.noreply.github.com>
2023-07-29 13:55:17 +0200

    fix typo, thanks to u/SaleB81

--- pages/docs/installation/docker-compose.vue
+++ pages/docs/installation/docker-compose.vue
@@ -14,7 +14,7 @@
             <Comment>"enable" the .env file to be detected/used by docker-compose</Comment>
         </li>
         <li>
-            optional: <b>only</b> change in your <code>.env</code> file <code>APP_PORT</code>  
+            optional: <b>only</b> change in your <code>.env</code> file <code>APP_PORT</code>
             if you wish to run this app under a different port
         </li>
         <li>
@@ -43,8 +43,8 @@
     <SubHeadline>Volumes</SubHeadline>
 
     <p>The docker-compose file uses a named volume to persist data.
-The reason for that is that bind mounts are more error-prone when it comes to 
-permissions and terrible when it comes to inserting data beforehand.</p>
+        The reason for that is that bind mounts are more error-prone when it comes to
+        permissions and terrible when it comes to inserting data beforehand.</p>
     <p>However if you must use bind mounts do the following:</p>
     <ul>
         <li>change the docker-compose file by (un)comment this section</li>
@@ -60,22 +60,24 @@ permissions and terrible when it comes to inserting data beforehand.</p>
             <Comment>access app container as root</Comment>
         </li>
         <li>
-            <code>chown -R www-data:www-data storage && chown -R 774 storage</code>
+            <code>chown -R www-data:www-data storage && chmod -R 774 storage</code>
             <Comment>Make this directory writable for the webserver</Comment>
         </li>
     </ul>
 
     <SubHeadline>Migrating from v2.6 and earlier</SubHeadline>
-    <p>Short answer: You only have to make sure that the <code>storage</code> directory is owned by www-data (see <a href="#volumes">Volume section</a> above).</p>
+    <p>Short answer: You only have to make sure that the <code>storage</code> directory is owned by www-data (see <a
+            href="#volumes">Volume section</a> above).</p>
     <p>Long answer: If you keep your existing docker-compose.yml you are going to use bind mounts
-which should not be a problem for you since it had to work so far and the new image uses the same 
-exact paths as before. Also you are going to use MySQL instead of PostgreSQL but that should not be a 
-problem either. <br>
-However if you would like to adapt and use the new docker-compose.yml file
-you need to start the container once before you use it to let the docker engine create and bootstrap 
-the volume for you. After that you can copy all subfolders of <code>data</code> into the newly
-created volume which might be located at <code>/var/lib/docker/volumes/yourBenotesInstallationDirectoryName_benotes_storage/_data</code>
- - next to the database volume. (You can also use <code>docker volume ls</code> and <code>docker volume inspect</code>
-to locate it.)</p>
-
+        which should not be a problem for you since it had to work so far and the new image uses the same
+        exact paths as before. Also you are going to use MySQL instead of PostgreSQL but that should not be a
+        problem either. <br>
+        However if you would like to adapt and use the new docker-compose.yml file
+        you need to start the container once before you use it to let the docker engine create and bootstrap
+        the volume for you. After that you can copy all subfolders of <code>data</code> into the newly
+        created volume which might be located at
+        <code>/var/lib/docker/volumes/yourBenotesInstallationDirectoryName_benotes_storage/_data</code>
+        - next to the database volume. (You can also use <code>docker volume ls</code> and
+        <code>docker volume inspect</code>
+        to locate it.)</p>
 </template>
